4847aa53d1e9872847395644000d170ba4921cc3
Jcc <1094404160@qq.com>
2021-05-08 19:21:56 +0800

    jenkins真难用

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-      <p>java1026 全体同学 行走的offer收割机</p>
-      <p>java1026 全体同学 前程似锦</p>
+      <p>java1026 小赵同学 jenkins跑起来</p>
+      <p>java1026 小赵同学 月薪过万</p>
       <p>java1026 小赵同学 第一天面试,第二天入职,第三天进组,第四天转正</p>
       <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3101694723,748884042&fm=26&gp=0.jpg"/>
       <img src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=316865976,2940650346&fm=26&gp=0.jpg"/>
